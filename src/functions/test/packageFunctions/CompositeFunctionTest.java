84386ec853549eba384a91ae35b00662c77f9fe6
MrFace55 <Serebryakov322@gmail.com>
2023-10-30 21:41:30 +0400

    загружаю пачку

--- src/functions/test/packageFunctions/CompositeFunctionTest.java
+++ src/functions/test/packageFunctions/CompositeFunctionTest.java
@@ -7,15 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CompositeFunctionTest {
-    //1) y=arcsin(sin(x))
-    //2) y=cos(x)
-    double[] xValuesArc = {0.0, Math.PI/6, Math.PI/4, Math.PI/3, Math.PI/2, 2*Math.PI/3, 3*Math.PI/4, 5*Math.PI/6, Math.PI};
-    double[] yValuesArc = {0.0, Math.PI/6, Math.PI/4, Math.PI/3, Math.PI/2, 2*Math.PI/3, 3*Math.PI/4, 5*Math.PI/6, Math.PI};
-    double[] xValuesCos = {0.0, Math.PI/6, Math.PI/4, Math.PI/3, Math.PI/2, 2*Math.PI/3, 3*Math.PI/4, 5*Math.PI/6, Math.PI};
-    double[] yValuesCos = {1.0, 0.866025, 0.707107, 0.5, 0, -0.5, -0.707107, -0.866025, -1.0};
+    @Test
+    public void testCompositeFunction() {
+        // Создаем сложные функции
+        MathFunction f = new CosinesFunction();
+        MathFunction g = new ArcsinSinFunction();
 
-    MathFunction f = new CosinesFunction();
-    MathFunction g = new ArcsinSinFunction();
+        // Создаем сложную функцию h(x) = g(f(x))
+        CompositeFunction h = new CompositeFunction(f,g);
 
+        // Тестируем применение сложной функции
+        assertEquals(0.866025, h.apply(Math.PI/6), 1e-10);
+        assertEquals(-1.0, h.apply(Math.PI), 1e-10);
+        assertEquals(0.5, h.apply(Math.PI/3), 1e-10);
+        assertEquals(0.0, h.apply(Math.PI/2), 1e-10);
 
+    }
 }
