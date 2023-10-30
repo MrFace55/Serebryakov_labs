c51defaa135166e940170442dd975b896848f1a8
MrFace55 <Serebryakov322@gmail.com>
2023-10-30 22:52:21 +0400

    загружаю пачку

--- src/functions/test/packageFunctions/ConstantFunctionTest.java
+++ src/functions/test/packageFunctions/ConstantFunctionTest.java
@@ -5,10 +5,15 @@ import packetFunctions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ConstantFunctionTest {
     private final ConstantFunction ConstantFunction = new ConstantFunction(5);
+    private final ZeroFunction ZeroFunction = new ZeroFunction();
+    private final UnitFunction UnitFunction = new UnitFunction();
 
     @Test
     public void testApply(){
         assertEquals(5.0, ConstantFunction.apply(-10.0), 0.000001);
         assertEquals(5.0, ConstantFunction.apply(11.0), 0.000001);
+        assertEquals(0.0, ZeroFunction.apply(10.0), 0.000001);
+        assertEquals(1.0, UnitFunction.apply(111.0), 0.000001);
+
     }
 }
